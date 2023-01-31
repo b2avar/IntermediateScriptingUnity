0baa69c4d2ad8fc1d0759c74cf26d6d92c5a0978
Burak Avar <burakavar@gmail.com>
2023-01-31 15:50:57 +0300

    Inheritance files added

---
+++ Assets/Scripts/Inheritance/FruitSalad.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IntermediateScripting
+{
+    public class FruitSalad : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Creating the fruit");
+            Fruit myFruit = new Fruit();
+            Debug.Log("Creating the apple");
+            Apple myApple = new Apple();
+            
+            //Call the methods of the Fruit class.
+            myFruit.SayHello();
+            myFruit.Chop();
+            
+            //Call the methods of the Apple class.
+            //Notice how class Apple has access to all
+            //of the public methods of class Fruit.
+            myApple.SayHello();
+            myApple.Chop();
+            
+            
+            //Now let's illustrate inheritance with the 
+            //constructors that read in a string.
+            Debug.Log("Creating the fruit");
+            myFruit = new Fruit("yellow");
+            Debug.Log("Creating the apple");
+            myApple = new Apple("green");
+            
+            //Call the methods of the Fruit class.
+            myFruit.SayHello();
+            myFruit.Chop();
+            
+            //Call the methods of the Apple class.
+            //Notice how class Apple has access to all
+            //of the public methods of class Fruit.
+            myApple.SayHello();
+            myApple.Chop();
+        }
+    }
+}
